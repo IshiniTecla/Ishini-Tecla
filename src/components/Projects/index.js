c2afecf428bf81021fbd327d49386cf2e23beab7
Ishini Tecla Warnasooriya <teclaishini442@gmail.com>
2025-04-03 16:44:06 +0530

    first commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  CardContainer,
+  ToggleButtonGroup,
+  ToggleButton,
+  Divider,
+} from "./ProjectsStyle";
+import ProjectCard from "../Cards/ProjectCards";
+import { projects } from "../../data/constants";
+
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
+
+  const filteredProjects =
+    toggle === "all"
+      ? projects
+      : projects.filter((item) => item.category === toggle);
+
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From web apps to android
+          apps. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup>
+          {["all", "web app", "android app"].map((category) => (
+            <React.Fragment key={category}>
+              <ToggleButton
+                active={toggle === category ? "true" : "false"} // Ensure active is a string
+                value={category}
+                onClick={() => setToggle(category)}
+              >
+                {category.toUpperCase().replace(" APP", " APP'S")}
+              </ToggleButton>
+              <Divider />
+            </React.Fragment>
+          ))}
+        </ToggleButtonGroup>
+        <CardContainer>
+          {filteredProjects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              project={project}
+              openModal={openModal}
+              setOpenModal={setOpenModal}
+            />
+          ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Projects;
